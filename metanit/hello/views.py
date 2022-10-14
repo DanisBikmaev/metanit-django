dcf48449c422d63d15400ad15fd4940431081370
Danis <bikmaevdv@gmail.com>
2022-10-14 14:48:51 +0500

    add pages

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
   
 def index(request):
-    return HttpResponse("Hello METANIT.COM")
+    return HttpResponse('<h2>Main</h2>')
+
+def about(request):
+    return HttpResponse('<h2>About</h2>')
+
+def contact(request): 
+    return HttpResponse('<h2>Contact</h2>')
+
+
