238b6ae209287a6f554153f7ada627ff8303404b
Danis <bikmaevdv@gmail.com>
2022-10-14 15:25:26 +0500

    get info

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -4,10 +4,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
   
 def index(request):
-    return HttpResponse('<h2>Main</h2>')
+    host = request.META['HTTP_HOST'] # server adres
+    user_agent = request.META['HTTP_USER_AGENT'] # browser date
+    path = request.path # path
+    return HttpResponse(f'''<h2>Main</h2>
+                        <p>Host: {host}</p>
+                        <p>User-agent: {user_agent}</p>
+                        <p>Path: {path}</p>
+                        ''')
 
-def about(request):
-    return HttpResponse('<h2>About</h2>')
+def about(request, name, age):
+    return HttpResponse(f'''<h2>About</h2>
+                        <p><b>Name:</b> {name}</p>
+                        <p><b>Age:</b> {age}</p>
+                        ''')
 
 def contact(request): 
     return HttpResponse('<h2>Contact</h2>')
--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from hello import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('about', views.about, name="about"),
-    path('contact', views.contact, name="contact"),
+    re_path(r'^about', views.about, kwargs={'name':'Tom', 'age':23}, name='about'),
+    re_path(r'^contact', views.contact, name='contact'),
 ]
